6301f81cf53d202405b28b4203b2053928276fa7
Ilya <ilya_rogachev_89@mail.ru>
2022-03-14 18:41:28 +0300

    Task_2 ready

---
+++ 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите целое положительное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+int sum = 0;
+for(int i=0; i<array.Length; i++)
+{
+array[i] = i+1;
+sum+=array[i];
+}
+Console.WriteLine(sum);
