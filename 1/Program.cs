c5961c0891aa4f245305a0483e4256ea8d8dc69b
Ilya <ilya_rogachev_89@mail.ru>
2022-03-14 18:33:08 +0300

    Task_1 ready

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти кубы чисел от 1 до N
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+CreateArray(array);
+
+void CreateArray(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        arr[i] = (int)Math.Pow(i+1, 3);
+        Console.Write(arr[i] + " ");
+    }
+}
