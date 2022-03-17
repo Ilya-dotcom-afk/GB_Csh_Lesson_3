d2270620326a716df37f874abc3274dcd01fa829
Ilya <ilya_rogachev_89@mail.ru>
2022-03-17 09:09:10 +0300

    Task_5 ready

---
+++ 5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] array = new int[8];
+Random rand = new Random();
+for(int i = 0; i<array.Length; i++)
+    {
+        array[i] = rand.Next(0, 2);
+        Console.Write(array[i] + " ");
+    }
