5e28ba695cbe1c94b6d795aaaea1e50ee4dbd8f7
Ilya <ilya_rogachev_89@mail.ru>
2022-03-17 07:09:36 +0300

    Task_4 ready

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите целое положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = 1;
+for(int i = 1; i<=n; i++)
+    {
+        s = s*i;
+    }
+Console.WriteLine(s);
