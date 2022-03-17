6e800c8011719e14a315777f78d6fe71f04a06a3
Ilya <ilya_rogachev_89@mail.ru>
2022-03-17 06:40:42 +0300

    Tast

---
+++ 3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int order = (int)Math.Log10(n) + 1;
+int[] array = new int[order];
+int sum = 0;
+for (int i=0; i<array.Length; i++)
+    {
+    int a = n%(int)Math.Pow(10, order-i);
+    int b = n%(int)Math.Pow(10, order-i-1);
+    int c = (int)Math.Pow(10, order-i-1);
+    array[i] = (a-b)/c;
+    sum += array[i];
+    }
+Console.Write(sum);
